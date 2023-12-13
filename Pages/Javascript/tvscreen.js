a3a3b8767b87bf648d3c8dbc55a1c4c91e938501
livknapen <59344183+livknapen@users.noreply.github.com>
2023-12-13 13:09:38 +0100

    tv screen added

--- Pages/Javascript/tvscreen.js
+++ Pages/Javascript/tvscreen.js
@@ -1,63 +1,82 @@
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-// We need 3 things everytime we use Three.js
- // Scene + Camera + Renderer
- const scene = new THREE.Scene()
- const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
- const renderer = new THREE.WebGLRenderer({ antialias: true})
- 
- renderer.setSize( window.innerWidth, window.innerHeight )
- // sets renderer background color
- renderer.setClearColor(0xffffff)
- document.body.appendChild( renderer.domElement )
- camera.position.z = 7
- 
- // resize canvas on resize window
- window.addEventListener( 'resize', () => {
-   let width = window.innerWidth
-   let height = window.innerHeight
-   renderer.setSize( width, height )
-   camera.aspect = width / height
-   camera.updateProjectionMatrix()
- })
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
 
- // Instantiate a loader
-const loader = new GLTFLoader();
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-// Optional: Provide a DRACOLoader instance to decode compressed mesh data
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath( 'Media/tv_screen/' );
-loader.setDRACOLoader( dracoLoader );
+document.body.appendChild(renderer.domElement);
 
-// Load a glTF resource
-loader.load(
-	// resource URL
-	'Media/tv_screen/tv_screen.glb',
-	// called when the resource is loaded
-	function ( gltf ) {
+const scene = new THREE.Scene();
 
-		scene.add( gltf.scene );
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0, 0, 0);
 
-		gltf.animations; // Array<THREE.AnimationClip>
-		gltf.scene; // THREE.Group
-		gltf.scenes; // Array<THREE.Group>
-		gltf.cameras; // Array<THREE.Camera>
-		gltf.asset; // Object
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 4;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 0.7, 0);
+controls.update();
 
-	},
-	// called while loading is progressing
-	function ( xhr ) {
+const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+groundGeometry.rotateX(-Math.PI / 2);
+const groundMaterial = new THREE.MeshStandardMaterial({
+  color: 0x555555,
+  side: THREE.DoubleSide
+});
+const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+groundMesh.castShadow = false;
+groundMesh.receiveShadow = true;
+scene.add(groundMesh);
 
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+const spotLight = new THREE.SpotLight(0xffffff,  3, 100, 0.22, 1);
+spotLight.position.set(1, 25, 1);
+spotLight.castShadow = true;
+spotLight.shadow.bias = -0.0001;
+scene.add(spotLight);
 
-	},
-	// called when loading has errors
-	function ( error ) {
+const loader = new GLTFLoader().setPath('/node_modules/flat_screen_television/');
+loader.load('scene.gltf', (gltf) => {
+  const mesh = gltf.scene;
 
-		console.log( 'An error happened' );
+  mesh.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
 
-	}
-);
+  mesh.position.set(0, 0.5, 0);
+  scene.add(mesh);
+
+  document.getElementById('progress-container').style.display = 'none';
+}, ( xhr ) => {
+  document.getElementById('progress').innerHTML = `LOADING ${Math.max(xhr.loaded / xhr.total, 1) * 100}/100`;
+},);
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
